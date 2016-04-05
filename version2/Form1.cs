5fad0778bfc7610e4dc96560cb6b5a647f7f5e3d
Taylor Rowan <taylorrowan86@gmail.com>
2016-04-05 14:29:24 -0500

    Changed Form
    Added labels, buttons, combo boxes

--- version2/Form1.cs
+++ version2/Form1.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace version2
 {
-    public partial class Form1 : Form
+    public partial class MainMenuForm : Form
     {
-        public Form1()
+        public MainMenuForm()
         {
             InitializeComponent();
+
+        }
+
+        //Player vs Player Checkbox
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainMenuForm_Load(object sender, EventArgs e)
+        {
+            PvCCheckbox.Select();
+        }
+
+        private void PvCCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
